e2690d9f5f8f191f49b24b2e3565cedad38c80c4
Maycon Manini <maycon.manini@yahoo.com>
2024-08-11 20:50:04 -0300

    Organizando e outra aula

---
+++ Desenvolvimento Dinâmico/Outras Aulas/Tipos de Dados.js
@@ -0,0 +1,17 @@
+var nome = 'Maycon';
+console.log(nome,typeof(nome)); //string
+
+var n;
+console.log(n, typeof(n)); //undefined
+
+var x = 42;
+console.log(x, typeof(x)); //number
+
+var True_or_False = true;
+console.log(True_or_False, typeof(True_or_False)); //boolean
+
+var key = null;
+console.log(key, typeof(key)); //object
+
+var key_2 = new Array();
+console.log(key_2, typeof(key_2)); //object
