dbdfd997fd22ee747e7d4c7c07731c50dc605ded
Maycon Manini <maycon.manini@yahoo.com>
2024-09-01 09:50:39 -0300

    Create Funcoes.js

---
+++ Desenvolvimento Dinâmico/Outras Aulas/Funcoes.js
@@ -0,0 +1,12 @@
+var nome = ''
+function Salve(nome){
+    console.log("Salve " + nome)
+}
+Salve('cleito')
+
+function soma(x,y,z){
+    let total = x+y+z
+    return total
+}
+let a = soma(2,3,5)
+console.log(a)
