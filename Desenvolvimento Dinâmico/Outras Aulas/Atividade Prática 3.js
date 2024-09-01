b800ad7c4de3352774e8dc7e00aa4c4c7b95abfe
Maycon Manini <maycon.manini@yahoo.com>
2024-09-01 12:18:03 -0300

    Create Atividade Prática 3.js

---
+++ Desenvolvimento Dinâmico/Outras Aulas/Atividade Prática 3.js
@@ -0,0 +1,42 @@
+class Funcionario{
+    constructor(nome, sobrenome, idade, cargo){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.nomecompleto = nome + " " +sobrenome;
+        this.idade = idade;
+        this.cargo = cargo;
+    }
+    seApresentar(){
+        console.log('Olá, me chamo', this.nomecompleto);
+        console.log('Eu sou', this.cargo);
+    }
+    trabalhar(){
+        //Fazer algo dentro da sua função.
+    }
+}
+
+class Gerente extends Funcionario{
+    constructor(nome, sobrenome, idade, departamento){
+        super(nome, sobrenome, idade);
+        this.cargo = "Gerente";
+        this.departamento = departamento;
+    }
+    gerenciar(){
+        console.log('Precisamos marcar uma reunião')
+    }
+}
+
+class Desenvolvedor extends Funcionario{
+    constructor(nome, sobrenome, idade, linguagem){
+        super(nome,sobrenome,idade);
+        this.cargo = "Desenvolvedor";
+        this.linguagem = linguagem;
+    }
+    programar(){
+        console.log("Esqueci do ponto e vírgula;")
+        console.log('Se compilou é porque roda')
+    }
+}
+
+const teste = new Desenvolvedor("Jorge", "Amaro", 45, "C#")
+teste.programar()
