f66adc2250b4f021b8b66380611f3574f1e6edfe
Maycon Manini <maycon.manini@yahoo.com>
2024-09-02 19:20:35 -0300

    Não aguento mais esse botão não funcionando

--- Desenvolvimento Dinâmico/Outras Aulas/Atividade Prática 4/Atividade Prática 4.js
+++ Desenvolvimento Dinâmico/Outras Aulas/Atividade Prática 4/Atividade Prática 4.js
@@ -1,91 +1,90 @@
-class Funcionario{
-    constructor(nome, sobrenome, idade, cargo){
+class Funcionario {
+    constructor(nome, sobrenome, idade, cargo) {
         this.nome = nome;
         this.sobrenome = sobrenome;
-        this.nomecompleto = nome + " " +sobrenome;
+        this.nomecompleto = nome + " " + sobrenome;
         this.idade = idade;
         this.cargo = cargo;
     }
-    seApresentar(){
+
+    seApresentar() {
         console.log('Olá, me chamo', this.nomecompleto);
         console.log('Eu sou', this.cargo);
     }
-    trabalhar(){
-        //Fazer algo dentro da sua função.
+
+    trabalhar() {
+        // Fazer algo dentro da sua função.
     }
 }
 
-class Gerente extends Funcionario{
-    constructor(nome, sobrenome, idade, departamento){
+class Gerente extends Funcionario {
+    constructor(nome, sobrenome, idade, departamento) {
         super(nome, sobrenome, idade);
         this.cargo = "Gerente";
         this.departamento = departamento;
     }
-    gerenciar(){
-        console.log('Precisamos marcar uma reunião')
+
+    gerenciar() {
+        console.log('Precisamos marcar uma reunião');
     }
 }
 
-class Desenvolvedor extends Funcionario{
-    constructor(nome, sobrenome, idade, linguagem){
-        super(nome,sobrenome,idade);
+class Desenvolvedor extends Funcionario {
+    constructor(nome, sobrenome, idade, linguagem) {
+        super(nome, sobrenome, idade);
         this.cargo = "Desenvolvedor";
         this.linguagem = linguagem;
     }
-    programar(){
-        console.log("Esqueci do ponto e vírgula;")
-        console.log('Se compilou é porque roda')
+
+    programar() {
+        console.log("Esqueci do ponto e vírgula;");
+        console.log('Se compilou é porque roda');
     }
 }
 
-const D1 = new Desenvolvedor("Jorge", "Amaro", 45, "C#")
-D1.programar()
+function mudando() { // Função para o HTML
+    let outraLangP = document.getElementById('outraLangP');
+    let outraLangR = document.getElementById('outraLangR');
+    let linguagemProgramacao = document.getElementById('lang').value;
 
-const G1 = new Gerente("Celso", "Portioli", 57, "Geral")
-G1.gerenciar()
-
-//Até aqui, é da atividade anterior; não tiveram mudanças
-    function mudando(){ //Função Para o HTML
-        let outraLangP = document.getElementById('outraLangP');
-        let outraLangR = document.getElementById('outraLangR');
-        let linguagemProgramacao = document.getElementById('lang').value;
-
-            if(linguagemProgramacao === 'Outra'){
-                outraLangP.style.display = 'block';
-                outraLangR.style.display = 'block';
-            } else{
-                outraLangP.style.display = 'none';
-                outraLangR.style.display = 'none';
-            }
+    if (linguagemProgramacao === 'Outra') {
+        outraLangP.style.display = 'block';
+        outraLangR.style.display = 'block';
+    } else {
+        outraLangP.style.display = 'none';
+        outraLangR.style.display = 'none';
     }
-//A partir daqui, testar em casa... Caso de errado, é daqui pra frente
-    function submit(){
-        var recebido_1 = new Funcionario(
-            this.nome = document.getElementsByName('nome')[0].value,
-            this.sobrenome = document.getElementsByName('sobrenome')[0].value,
-            this.idade = document.getElementsByName('idade')[0].value,
-            this.cargo = document.getElementsByName('cargo')[0].value
-        )
-            if (linguagemProgramacao === 'Outra'){
-                recebido_1.linguagem = document.getElementById('outraLangR')
-            } else{
-                recebido_1.linguagem = getElementsByName('lang');  
-            }
-        window.alert ('Registrado, obrigado ' + this.nomecompleto)
-        try{
-            if(idade<14) throw 'Confira sua idade. Você é muito novo para trabalhar aqui';
-            if(idade>85) throw 'O(a) senhor(a) já deveria estar aposentado(a)';
-            if(typeof(cargo) != 'string') throw 'Por gentileza, confira o cargo inserido; ele pode estar errado';
-        }catch(err){
-            mensage.innerHTML = 'Opa, temos um erro... '
-        }
+}
+
+function submit(event) {
+    event.preventDefault();
+
+    const nome = document.getElementsByName('nome')[0].value;
+    const sobrenome = document.getElementsByName('sobrenome')[0].value;
+    const idade = document.getElementsByName('idade')[0].value;
+    const cargo = document.getElementsByName('cargo')[0].value;
+    const linguagemProgramacao = document.getElementById('lang').value;
+
+    let funcionario;
+
+    if (cargo.toLowerCase() === 'gerente') {
+        const departamento = document.getElementsByName('departamento')[0].value;
+        funcionario = new Gerente(nome, sobrenome, idade, departamento);
+    } else if (cargo.toLowerCase() === 'desenvolvedor') {
+        funcionario = new Desenvolvedor(nome, sobrenome, idade, linguagemProgramacao);
+    } else {
+        funcionario = new Funcionario(nome, sobrenome, idade, cargo);
     }
 
+    funcionario.seApresentar();
 
+    alert('Registrado, obrigado ' + funcionario.nomecompleto); //não tá aparecendo... não consegui
 
-
-
-
-
-
-
+    try {
+        if (idade < 14) throw 'Confira sua idade. Você é muito novo para trabalhar aqui';
+        if (idade > 85) throw 'O(a) senhor(a) já deveria estar aposentado(a)';
+        if (typeof cargo !== 'string') throw 'Por gentileza, confira o cargo inserido; ele pode estar errado';
+    } catch (err) {
+        alert('Opa, temos um erro: ' + err);
+    }
+}
